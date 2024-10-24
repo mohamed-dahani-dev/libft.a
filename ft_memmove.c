6fd8b6fd0890810af27bcb6cfd1ad5a8bb51256a
Mohamed Dahani <mdahani@e1r7p4.1337.ma>
2024-10-24 18:30:14 +0100

    simplify the ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 18:39:49 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/24 17:18:00 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/10/24 18:26:13 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 	i = 0;
 	if (tmp_dest > tmp_src)
 	{
-		while (len-- > 0)
+		while (len > 0)
+		{
 			tmp_dest[len] = tmp_src[len];
+			len--;
+		}
 	}
 	else
 	{
