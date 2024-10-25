1c94ef28a2e15a55c3003153e632e12c17e27bf4
Mohamed Dahani <mdahani@e1r7p4.1337.ma>
2024-10-25 15:20:01 +0100

    add int main in ft_strchar.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:50:44 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/23 11:06:35 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/10/25 15:18:51 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,19 @@ char	*ft_strchr(const char *str, int c)
 		return ((char *) &str[i]);
 	return (0);
 }
+
+/*
+int main() {
+    const char *str = "Hello, world!";
+    char c = 'w';
+
+    char *result = ft_strchr(str, c);
+    if (result) {
+        printf("Found %c at position: %i", c);
+    } else {
+        printf("'%c' not found.\n", c);
+    }
+
+    return 0;
+}
+*/
