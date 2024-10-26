c049dc3d9b441efc26cf635d88090d5c82b4fe45
Mohamed Dahani <mdahani@e2r12p2.1337.ma>
2024-10-26 14:45:20 +0100

    add ft_strnstr.c to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/26 12:22:53 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/10/26 14:44:57 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ char	*ft_strrchr(const char *str, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *str, const char *find, size_t len);
 
 #endif
