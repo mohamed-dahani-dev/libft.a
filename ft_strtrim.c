6482e30a48a9f1594bed8ab3f73a1521a145942c
Mohamed Dahani <mdahani@e2r11p4.1337.ma>
2024-10-27 17:19:33 +0100

    add ft_strjoin.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,37 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 11:25:22 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/27 16:21:00 by mdahani          ###   ########.fr       */
+/*   Created: 2024/10/27 15:21:07 by mdahani           #+#    #+#             */
+/*   Updated: 2024/10/27 15:24:00 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft.a"
 
-void	*ft_memset(void *str, int c, size_t len)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	unsigned char	*tmp_ptr;
-	size_t			i;
-
-	tmp_ptr = (unsigned char *) str;
-	i = 0;
-	while (i < len)
-	{
-		tmp_ptr[i] = (unsigned char) c;
-		i++;
-	}
-	return (str);
+	char	*str;
+	size_t	i;
+	
+	str = malloc()
 }
-/*
-int main()
-{
-    char str[50] = "hello world!";
-    ft_memset(str, '*', 5);
-    printf("%s\n", str);
-    return 0;
-}
-*/
