11cd3c47d212bd2190eee4a03fc4c9cc5508bbb6
Mohamed Dahani <mdahani@e2r11p3.1337.ma>
2024-10-28 11:46:39 +0100

    change malloc from +2 to +1

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:08:40 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/27 16:08:44 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/10/28 11:45:27 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
-	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 2);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (NULL);
 	i = 0;
