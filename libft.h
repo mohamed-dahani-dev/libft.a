efcbc7cf131ffc6ffbd85f24e7ebd822c897ce66
Mohamed Dahani <mdahani@e1r11p12.1337.ma>
2024-10-30 13:46:35 +0100

    delete ft_split.c and start ft_itoa.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/29 12:25:51 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/10/29 17:30:18 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
