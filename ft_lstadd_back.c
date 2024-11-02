d2279888e1a0aee925c81e05a9fd9559e676be04
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-02 12:50:52 +0100

    add ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
+/*   Updated: 2024/11/02 12:49:25 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst)
+		return (NULL);
+	last = *lst;
+	while (last -> next != NULL)
+		last = last -> next;
+	new = last -> next;
+}
