f23df60fa76f665e82d36d9456510e5df8b82edc
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-02 20:29:38 +0100

    fix ft_lstclear_bonus.c

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 20:26:45 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/02 20:29:25 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
 
-	if (!lst)
+	if (!lst || !*lst)
 		return ;
-	while (lst != NULL)
+	while (*lst != NULL)
 	{
 		tmp = (*lst)-> next;
 		del((*lst)-> content);
-		free(lst);
-		lst = tmp;
+		free(*lst);
+		*lst = tmp;
 	}
 	*lst = NULL;
 }
