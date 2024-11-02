8d555648e7d8164b6c662829909b14c9b97c2bc0
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-02 21:43:09 +0100

    fix ft_lstmap_bonus.c

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 19:47:59 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/02 21:42:27 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst || !f)
 		return (NULL);
 	new = NULL;
-	while (!lst)
+	while (lst != NULL)
 	{
 		current = ft_lstnew(f(lst -> content));
 		if (!current)
 		{
-			ft_lstclear(&current, (*del));
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new, current);
