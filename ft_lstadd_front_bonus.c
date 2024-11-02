c3c757d442cda9dfc948174980e2cbf6ed50cb27
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-02 20:12:47 +0100

    fix ft_lstadd_front_bonus.c

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 20:10:03 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/02 20:12:35 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst)
+	if (!lst || !new)
 		return ;
-	new -> next = lst;
-	lst = new;
+	new -> next = *lst;
+	*lst = new;
 }
