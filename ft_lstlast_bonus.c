8fb0afc021a326de7835de5acf883b693f08fea8
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 17:49:20 +0100

    add int main ft_lstlast_bonus.c

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/01 19:05:01 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 17:48:15 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,16 @@ t_list	*ft_lstlast(t_list *lst)
 		lst = lst -> next;
 	return (lst);
 }
+
+/*
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	t_list *element3 = ft_lstnew("Hello World 3");
+	ft_lstadd_front(&element, element2);
+	ft_lstadd_front(&element, element3);
+	printf("%s\n", ft_lstlast(element) -> content);
+	return 0;
+}
+*/
