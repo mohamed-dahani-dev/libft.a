fa2bf70e809323d0583ec450e6aec42558d432bc
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 17:12:51 +0100

    add int main ft_lstadd_front_bonus.c

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 20:12:35 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 17:11:56 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,15 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new -> next = *lst;
 	*lst = new;
 }
+
+/*
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	ft_lstadd_front(&element, element2);
+	printf("%s\n", element -> content);
+	printf("%s\n", element -> next -> content);
+	return 0;
+}
+*/
