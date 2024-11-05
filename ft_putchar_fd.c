63b5c72e4608547eef1e5f3929adbcd53b3caad7
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 13:03:33 +0100

    add in main in ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/31 11:50:14 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 12:59:27 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	ft_putchar_fd(char c, int fd)
 
 /*
 int main() {
-    ft_putchar_fd('a', 1);
+    FILE *file = fopen("file.txt", "w");
+    ft_putchar_fd('b', fileno(file));
+    fclose(file);
     return 0;
 }
 */
