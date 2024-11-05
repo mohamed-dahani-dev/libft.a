87aaaf04425ba8311a33737f9c641c0801ee6a94
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 17:45:37 +0100

    add int main ft_lstsize_bonus.c

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/05 16:01:57 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 17:44:57 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,16 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
+
+/*
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	t_list *element3 = ft_lstnew("Hello World 3");
+	ft_lstadd_front(&element, element2);
+	ft_lstadd_front(&element, element3);
+	printf("%d\n", ft_lstsize(element));
+	return 0;
+}
+*/
