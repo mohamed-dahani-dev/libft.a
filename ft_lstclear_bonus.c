40785643ae064bf6125b59cf30360d440d5ecf07
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 18:28:38 +0100

    add int main ft_lstclear_bonus.c

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/05 16:04:11 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 18:28:04 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,15 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	}
 	*lst = NULL;
 }
+
+/*
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	ft_lstadd_front(&element, element2);
+	ft_lstclear(&element, free);
+	printf("%s\n", element -> content);
+	return 0;
+}
+*/
