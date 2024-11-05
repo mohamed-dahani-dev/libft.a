18cb47c012ea40af9293b71a488d9a92cfa3e5a8
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 19:02:10 +0100

    add int main ft_lstiter_bonus.c

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 20:30:13 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 19:01:37 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,21 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst -> next;
 	}
 }
+
+/*
+void print_content(void *content)
+{
+	printf("%s\n", (char *)content);
+}
+
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	t_list *element3 = ft_lstnew("Hello World 3");
+	ft_lstadd_front(&element, element2);
+	ft_lstadd_front(&element, element3);
+	ft_lstiter(element, &print_content);
+	return 0;
+}
+*/
