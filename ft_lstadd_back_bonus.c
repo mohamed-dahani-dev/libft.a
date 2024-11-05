2a460620236bd9385606c3ff35490c5dca48263b
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 17:56:49 +0100

    add int main ft_lstadd_back_bonus.c

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/02 20:24:20 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 17:56:23 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,16 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		last -> next = new;
 	}
 }
+
+/*
+int main()
+{
+	t_list *element = ft_lstnew("Hello World");
+	t_list *element2 = ft_lstnew("Hello World 2");
+	t_list *element3 = ft_lstnew("Hello World 3");
+	ft_lstadd_back(&element, element2);
+	ft_lstadd_back(&element, element3);
+	printf("%s\n", element -> next -> next -> content);
+	return 0;
+}
+*/
