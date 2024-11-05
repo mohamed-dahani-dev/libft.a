dfb4b51842b7f253afed5e5acfc75f8cbd85a10b
Mohamed Dahani <mdahani@e1r11p10.1337.ma>
2024-11-05 19:29:11 +0100

    add int main ft_lstmap_bonus.c

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 10:28:07 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/04 19:48:37 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/05 19:28:40 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,41 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_list);
 }
+
+/*
+void *ft_strtoupper(void *str)
+{
+    char *original_str = (char *)str;
+    char *new_str = strdup(original_str);
+
+    if (new_str == NULL)
+        return NULL;
+
+    int i = 0;
+    while (new_str[i] != '\0')
+    {
+        new_str[i] = ((unsigned char)new_str[i] - 32);
+        i++;
+    }
+    return (void *)new_str;
+}
+
+void ft_free(void *content)
+{
+    free(content);
+}
+
+int main()
+{
+    t_list *lst = ft_lstnew("hello");
+    t_list *new_list = ft_lstmap(lst, ft_strtoupper, ft_free);
+    while (new_list)
+    {
+        printf("%s\n", (char *)new_list->content);
+        new_list = new_list->next;
+    }
+    ft_lstclear(&lst, ft_free);
+    ft_lstclear(&new_list, ft_free);
+    return 0;
+}
+*/
