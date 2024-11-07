2d3a7e7def205201c4634b104815cb53ee1b7da1
Mohamed Dahani <mdahani@e1r11p12.1337.ma>
2024-11-07 13:16:57 +0100

    fix strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:08:40 by mdahani           #+#    #+#             */
-/*   Updated: 2024/10/28 11:45:27 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/07 13:16:13 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
+	if (!s1 || !s2)
+		return (NULL);
 	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (NULL);
